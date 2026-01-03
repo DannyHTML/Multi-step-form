f7d3a1c9ffe237cf01663e07c36ba26937a1030d
Danny <dannyjag@gmail.com>
2026-01-03 22:05:45 +0100

    Test and added TODO

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
-
+<!-- TODO: Make setup correct before starting project. -->
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
