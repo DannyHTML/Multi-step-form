323e6c08b2a3a45fcafadbe01c1522c9b63f108c
Danny <dannyjag@gmail.com>
2026-01-15 23:27:00 +0000

    Removed TODO's

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,10 +6,6 @@ import PickAddOns from '@/views/PickAddOnsView.vue';
 import SummaryView from '@/views/SummaryView.vue';
 import { useStepValidation } from '@/stores/stepValidation';
 
-// TODO: Add route guards to protect the signup steps.
-// TODO: Also, remove the goNext/goPrev function from App.vue and handle navigation here instead.
-// TODO: Make this cleaner using a navigation store???.
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
